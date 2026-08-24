cb4387bbd5539c5fcb944a8f0466b85eb2952460
ohadmir <34608387+ohadmir@users.noreply.github.com>
2026-08-24 22:34:26 +0300

    Fix no-eval [evo-remediation] (#2)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 const apiKey = process.env.API_KEY ?? "";
 
 export function runUserScript(userInput: string) {
-  return eval(userInput);
+  throw new Error("Executing dynamic user-provided code is not allowed.");
 }
 
 export async function sendAnalytics(payload: unknown) {
